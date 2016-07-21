dbca0f2decb2030d603d6dcbfb36fe6717576717
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-07-21 15:26:43 +1000

    Fixed function error, fixes #1, also changed data formatting

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,7 @@ require 'mechanize'
 
 def get_page_elements(page)
   elements_in_div = page.search("div.col_4")
-  links_in_div = list_of_applications.search('a')
+  links_in_div = elements_in_div.search('a')
 
   return links_in_div
 end
@@ -25,11 +25,15 @@ def save_one_application(planning_application_page, url)
   elements_from_page = planning_application_page.search("div.col_4").children
   #On Wednesday July 20, each application was on a page in a single element broken up with <br> tags. For that reason, I've accessed the child elements directly.
 
+  on_notice_to_unformatted = elements_from_page[14].inner_text.strip
+  day, month, year = on_notice_to_unformatted.split("/")
+  on_notice_to_formatted = "#{year}-#{month}-#{day}"
+
   record = {
     "info_url" => url,
     "council_reference" => elements_from_page[4].inner_text.strip,
     "address" => elements_from_page[2].inner_text.strip,
-    "on_notice_to" => elements_from_page[14].inner_text.strip,
+    "on_notice_to" => on_notice_to_formatted,
     "description" => elements_from_page[9].inner_text.strip,
     "date_scraped" => Date.today.to_s
   }
@@ -48,7 +52,7 @@ agent = Mechanize.new
 first_page = agent.get(url)
 
 #This site has links to each planning application in one giant div. Each application is on a separate page
-links_on_main_div = get_planning_links(first_page)
+links_on_main_div = get_page_elements(first_page)
 links_to_planning_applications = return_only_application_links(links_on_main_div)
 
 links_to_planning_applications.each do |link|
