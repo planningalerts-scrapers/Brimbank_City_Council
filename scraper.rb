7e0fc1e2fc0f1d88cae35b14f045170c59907f15
Matthew Landauer <mlandauer@gmail.com>
2024-07-18 15:10:50 +1000

    Rewrite scraper for new site https://github.com/planningalerts-scrapers/issues/issues/912

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,28 @@
 require 'scraperwiki'
 require 'mechanize'
 
-url   = "https://www.brimbank.vic.gov.au"
+url = "https://www.brimbank.vic.gov.au/advertised-plans/current-advertised-plans"
 agent = Mechanize.new
-page  = agent.get(url + "/advertised-plans")
 
-page.search("div.item-list a").each do |a|
-  page.search("tr.odd, tr.even").each do |tr|
+page_no = 0
+
+loop do
+  page = agent.get("#{url}?page=#{page_no}")
+  
+  tbody = page.at("table tbody")
+  # If we're past the last page there is no table of results
+  break if tbody.nil?
+
+  tbody.search("tr").each do |tr|
     record = {
-      "council_reference" => tr.search("td")[1].inner_text.strip,
-      "address" => tr.search("td")[0].inner_text.strip + ", VIC",
-      "description" => tr.search("p")[0].inner_text.strip,
-      "info_url" => tr.search('a')[0].attributes['href'].to_s,
-      "comment_url" => "mailto:info@brimbank.vic.gov.au",
+      "council_reference" => tr.at("td.views-field-title").inner_text.strip,
+      "address" => tr.at("td.views-field-field-address").inner_text.strip + ", VIC",
+      "description" => tr.at("td.views-field-body").inner_text.strip,
+      "info_url" => tr.at("td.views-field-field-document-and-plans a")["href"],
       "date_scraped" => Date.today.to_s
     }
-
-    puts "Saving record " + record['council_reference'] + " - " + record['address']
-#      puts record
+    puts "Saving record #{record['council_reference']} - #{record['address']}" 
     ScraperWiki.save_sqlite(['council_reference'], record)
   end
-  page = agent.get(url + a.attributes['href'].to_s)
+  page_no += 1
 end
